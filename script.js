4efc96167f70feb627ae6bccb81b5dd05f842765
Fathir Muhamad Arrahman <fathirmar@gmail.com>
2025-10-28 20:32:07 +0700

    new

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 document.addEventListener("DOMContentLoaded", () => {
   const form = document.getElementById("loginForm");
   if (form) {
-    form.addEventListener("submit", e => {
+    form.addEventListener("submit", (e) => {
       e.preventDefault();
       const user = document.getElementById("username").value;
       const pass = document.getElementById("password").value;
@@ -19,7 +19,7 @@ document.addEventListener("DOMContentLoaded", () => {
 function searchBooks() {
   const searchValue = document.getElementById("search").value.toLowerCase();
   const books = document.querySelectorAll(".book-card");
-  books.forEach(book => {
+  books.forEach((book) => {
     const title = book.querySelector("h3").textContent.toLowerCase();
     book.style.display = title.includes(searchValue) ? "block" : "none";
   });
